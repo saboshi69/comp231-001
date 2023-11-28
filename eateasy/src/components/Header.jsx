ed920b589a7f40439c5a139c9c7d2df1bf10299d
saboshi69 <shui2967@gmail.com>
2023-11-28 17:43:47 -0500

    add logout function

--- eateasy/src/components/Header.jsx
+++ eateasy/src/components/Header.jsx
@@ -1,8 +1,22 @@
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { signOut } from "../util/Http";
+import { useNavigate } from "react-router-dom";
+import { setCurrentUser } from "../redux/feature/userSlice";
+
 
 const Header = () => {
   const currentUser = useSelector((state) => state.user.currentUser);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const signOutHandler = () => {
+    signOut();
+    dispatch(setCurrentUser(null));
+    navigate("/sign-in");
+  };
+
+
   return (
     <header className="bg-neutral text-neutral-content">
       <div className="flex justify-between mx-auto w-5/6 items-center p-3">
@@ -18,7 +32,12 @@ const Header = () => {
             <li className="md:inline hover:underline cursor-pointer">Offer</li>
           </Link>
           {currentUser ? (
-            <Link to="profile">{currentUser.username ? currentUser.username : ""}</Link>
+            <>
+              <Link to="profile">{currentUser.username ? currentUser.username : ""}</Link>
+              <span className="cursor-pointer hover:underline" onClick={signOutHandler}>
+                Logout
+              </span>
+            </>
           ) : (
             <Link to="sign-in">
               <li className=" hover:underline cursor-pointer">Sign In</li>
